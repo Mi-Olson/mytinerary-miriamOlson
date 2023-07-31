227b5c6a74c19f9f22b348e8568f4a6406ee50c4
Miriam <97313703+Mi-Olson@users.noreply.github.com>
2023-07-31 13:37:49 -0300

    primera entrega. falta terminar

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+
+// import './index.css'
+
+function App() {
+
+  return (
+    <>
+    
+
+        <div class='relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12'>
+     
+          <div class="flex items-center justify-between border-b  p-3">
+            {/* <div class="flex items-center space-x-2 rounded bg-gray-100 py-1 px-2 text-slate-500 shadow-md"> */}
+            <div class="text-lg font-bold text-black">My Tinary</div>
+              {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-4 w-4">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
+              </svg>
+              <span>My Tinary</span> */}
+            
+            {/* </div> */}
+          
+            <div class="flex items-center space-x-5 text-black">
+              
+                <div class="text-lg font-bold ">Home</div>
+                <div class="text-lg font-bold">Cities</div>
+                <button class="bg-violet-600  font-bold text-black px-4 py-2 rounded-md w-1/2">Login</button>
+                {/* <div class="flex items-center space-x-2 rounded bg-gray-100 py-1 px-2 text-slate-500 shadow-md">LOGIN</div>
+             */}
+              
+              
+            </div>
+          </div>
+         
+
+<body class="bg-gray-100">
+  <div class="min-h-screen flex items-center justify-center">
+    <div class="max-w-md w-full p-6 bg-white rounded-lg shadow-lg">
+      
+      <h1 class="text-2xl font-semibold text-center text-gray-500 mt-8 mb-6">Find the perfect destination</h1> 
+      <p class="text-sm text-gray-600 text-justify mt-8 mb-6">Al unirte a nuestro programa de fidelización y tokenización de puntos, conseguirás obtener puntos con cada compra y canjearlos por descuentos exclusivos, regalos especiales o convertirlos en criptomonedas.</p> 
+      <div class="flex justify-right space-x-4 my-4">
+        <button class="bg-violet-600 text-white px-4 py-2 rounded-md w-1/2">View More</button>
+       
+      </div>
+     
+    
+    </div>
+  </div>
+</body>
+
+        </div>
+        
+   
+
+      
+    </>
+  )
+}
+
+export default App
