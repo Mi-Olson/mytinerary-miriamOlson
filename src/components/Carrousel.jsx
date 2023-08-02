a6b6903e44a98ec85275d3ad470e656670ef7237
Miriam <97313703+Mi-Olson@users.noreply.github.com>
2023-08-02 13:32:13 -0300

    se cargó public , se crean ramas pertinentes

---
+++ src/components/Carrousel.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+export default function Carrousel() {
+    var data = [
+        {id: 'america1', city: "Cancun", photo:"/img/america/newyork.jpg" },
+        {id: 'america2', city: "New York", photo: "/img/america/newyork.jpg"},
+        {id: 'america3', city: "Rio de Janeiro", photo: "/img/america/rioDeJaneiro.jpg"},
+        {id: 'america4', city: "Ushuaia", photo: "/img/america/ushuaia.jpg"},
+        {id: 'asia1' , city: "Bangkok", photo: "/img/asia/bangkok.jpg"},
+        {id: 'asia2' , city: "Pekin", photo: "/img/asia/pekin.jpg"},
+        {id: 'asia3' , city: "Singapur", photo: "/img/asia/singapur.jpg"},
+        {id: 'asia4' , city: "Tokyo", photo: "/img/asia/tokio.jpg"},
+        {id: 'europe1' , city: "Ibiza", photo: "/img/europe/ibiza.jpg"},
+        {id: 'europe2' , city: "London", photo: "/img/europe/london.jpg"},
+        {id: 'europe3' , city: "Paris", photo: "/img/europe/paris.jpg"},
+        {id: 'europe4' , city: "Roma", photo: "/img/europe/roma.jpg"},
+        {id: 'oceania1' , city: "Majuro", photo: "/img/oceania/majuro.jpg"},
+        {id: 'oceania2' , city: "Sidney", photo: "/img/oceania/sidney.jpg"},
+        {id: 'oceania3' , city: "Suva", photo: "/img/oceania/suva.jpg"},
+        {id: 'oceania4' , city: "Wellington", photo: "/img/oceania/wellington.jpg"}
+    ]
+
+
+   
+
+
+
+  return (
+    <>
+ 
+ <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src={data[0].photo} class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src={data[1].photo} class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src={data[2].photo} class="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+     
+      </>
+  )
+  }
