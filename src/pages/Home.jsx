37a3f49c8d6cf5d15cf8ad7818018775d1fb9a25
Miriam <97313703+Mi-Olson@users.noreply.github.com>
2023-09-02 17:14:01 -0300

    se mogificó pagina de inicio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,36 +2,28 @@ import React from 'react'
 import Carousel from '../components/Carousel'
 import { useState, useEffect } from "react";
 import CardHome from '../components/CardHome'
-// import apiUrl from '../services/apiUrl'
-// import axios from "axios";
+
 import { useSelector,useDispatch } from 'react-redux';
 
 import city_actions from '../store/actions/cities';
 const {read_carousel}=city_actions
-
+import Marquee from '../components/Marquee';
 
 export default function Home() {
 
 
-    // const [data, setData] = useState([])
-    // const city_reducer=useSelector(store=>store.cities)
-    // console.log(city_reducer);
     const carousel =useSelector(store=>store.cities.carousel)
-    // console.log(carousel);
+   
     const dispatch =useDispatch()
     useEffect(
         () => {
             if (carousel.length===0) {
             dispatch(read_carousel())}
 
-            // axios(apiUrl+'cities/carousel')
-            //      .then(res => setData(res.data.data_carousel))
-            //     .catch(err => console.log(err))    
-        },        //callback que NO debe retornar nada y NO puede ser asincrona
-        []        //array de dependencias
-        //cuando está vacío EL EFECTO se ejecuta UNA UNICA VEZ cuando se monta el componente
-        //cuando tiene variables de dependencias EL EFECTO se ejecuta (cuando se monta y) CADA VEZ que varía/cambia alguna de esas variables
-        //[show]  //en este ejemplo CADA VEZ que el booleano CAMBIE, se ejecuta la caalback de la L28
+            
+        },      
+        []      
+       
     )
 
     return (
@@ -39,11 +31,12 @@ export default function Home() {
 
 
         <div className="container text-center min-vh-75 justify-content-center align-self-center "  >
+            <Marquee/>
             <div className='row'>
                 <div className='col-sm-12 col-md-3 col-lg-3 '>
                     <CardHome />
                 </div>
-
+               
                 <div className="col-sm-12 col-md-9 col-lg-9 flex items-center ">
                     <Carousel data={carousel} />
                 </div>
