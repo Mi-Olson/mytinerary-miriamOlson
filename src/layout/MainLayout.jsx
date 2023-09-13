2c186ff311589fa02d08ee7aa78831302892636c
Miriam <97313703+Mi-Olson@users.noreply.github.com>
2023-09-13 12:19:14 -0300

    cambiando el estilo de fondo

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -6,8 +6,18 @@ import { Outlet } from 'react-router-dom'
 
 
 export default function MainLayout() {
+  const image="https://images.unsplash.com/photo-1513002749550-c59d786b8e6c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Zm9uZG8lMjBkZSUyMHBhbnRhbGxhJTIwZGUlMjBudWJlc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
   return (
-    <div className='d-flex flex-column justify-content-between aling-content-between min-vh-100 multi_bg_example' >
+
+    <div className=" h-[100vh] bg-cover bg-center "  style={{
+      backgroundImage: `url(${image})`,
+      // backgroundImage: `url(${externalImage})`,
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+     
+    }}>
+    {/* <div className='d-flex flex-column justify-content-between aling-content-between min-vh-100 multi_bg_example' > */}
         <Header2/>
         <Outlet/>
        <Footer/>
